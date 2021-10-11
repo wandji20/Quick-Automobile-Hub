8d2c582c799fd916811cc6b2eac229b15f57586a
wandji20 <wbertrand10@gmail.com>
2021-10-11 20:04:48 +0100

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   get 'profile', to: 'users#show'
   put 'profile', to: 'users#update'
   delete 'profile', to: 'users#destroy'
+  resources :users, only: [:create]
   resources :ratings, only: [:create, :update]
   resources :sales, only: [:create, :show, :update, :destroy]
-  resources :cars, only: [:create, :show, :update, :destroy]
+  resources :cars, only: [:create, :show, :update, :destroy, :index]
   resources :hires, only: [:create, :show, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
